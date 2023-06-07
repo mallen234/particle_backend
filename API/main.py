ca16fa60a83338005b3a1a70c11eb552f252ddb7
Marcus Allen <marcusallen23@hotmail.co.uk>
2023-06-07 22:19:11 +0100

    added API logic to get data.json file

--- API/main.py
+++ API/main.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+import json
+import subprocess
+
 
 app = Flask("ParticleAPI")
 api = Api(app)
@@ -7,7 +10,22 @@ api = Api(app)
 class Video(Resource):
 
     def get(self):
-        return "Hello World!"
+
+        # subprocess.run(['python3', '../main.py'])
+
+        with open('../data.json') as f:
+            data = json.load(f)
+        return jsonify(data)
+    
+    @app.route('/api/data', methods=['GET'])
+    def get_json_data():
+
+        subprocess.run(['python3', '../main.py'])
+
+
+        with open('../data.json') as f:
+            data = json.load(f)
+        return jsonify(data)
     
 
 api.add_resource(Video, '/')
